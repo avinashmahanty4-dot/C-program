9c09542a2a001ade8d773c1777a1c231f71536b7
Avinash mohanty <avinashmahanty4@gmail.com>
2026-02-01 11:53:46 +0530

    frequency.c

---
+++ frequency.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+
+    gets(str);
+
+    for(int i=0; str[i]; i++)
+        freq[str[i]]++;
+
+    for(int i=0;i<256;i++)
+        if(freq[i])
+            printf("%c = %d\n", i, freq[i]);
+
+    return 0;
+}
